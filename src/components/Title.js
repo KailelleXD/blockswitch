ec6b491ff495c218c1fc78dd2fee77fd657fff8a
Steven Ng <40282847+MrStevenNg@users.noreply.github.com>
2019-04-07 16:36:29 -0700

    created Title.js

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class Title extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Title.js </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
