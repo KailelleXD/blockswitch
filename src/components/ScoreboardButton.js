d4b12d1cb47c316149f2117a0d715b7249d4a2b3
Steven Ng <40282847+MrStevenNg@users.noreply.github.com>
2019-04-07 16:15:00 -0700

    added ScoreboardButton.js

---
+++ src/components/ScoreboardButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class ScoreboardButton extends Component {
+  render() {
+    return (
+      <View>
+        <Text> ScoreboardButton.js </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
