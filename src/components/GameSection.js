1ab121101c4a2b9544651b04c1ed3c1d472c9dee
Steven Ng <40282847+MrStevenNg@users.noreply.github.com>
2019-04-07 16:41:24 -0700

    created GameSection.js

---
+++ src/components/GameSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class GameSection extends Component {
+  render() {
+    return (
+      <View>
+        <Text> GameSection.js </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
