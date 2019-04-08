0dd42c59dfbc9d84691be3dd0f5281bfe825da32
Steven Ng <40282847+MrStevenNg@users.noreply.github.com>
2019-04-07 23:21:47 -0700

    working on cross styling

--- src/components/Cross.js
+++ src/components/Cross.js
@@ -1,33 +1,42 @@
-import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
-import Block from './Block'
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { Grid, Col, Row } from "react-native-easy-grid";
+import Block from "./Block";
 
 export default class Cross extends Component {
   render() {
     return (
       <View>
-        <Block color={styles.darkBlue} />
-        <Block color={styles.goldenRod} />
-        <Block color={styles.orangeRed} />
+        <Row size={33}>
+        <Block color={styles.darkRed} />
+        </Row>
+        <Row size={33}>
+          <Block color={styles.darkBlue} />
+          <Block color={styles.goldenRod} />
+          <Block color={styles.orangeRed} />
+        </Row>
+        <Row size={33}>
+            <Block color={styles.darkGreen} />
+        </Row>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   darkRed: {
-    backgroundColor: '#8B0000'
+    backgroundColor: "#8B0000"
   },
   darkBlue: {
-    backgroundColor: '#00008B'
+    backgroundColor: "#00008B"
   },
   goldenRod: {
-    backgroundColor: '#DAA520'
+    backgroundColor: "#DAA520"
   },
   orangeRed: {
-    backgroundColor: '#FF4500'
+    backgroundColor: "#FF4500"
   },
   darkGreen: {
-    backgroundColor: '#006400'
+    backgroundColor: "#006400"
   }
-})
+});
