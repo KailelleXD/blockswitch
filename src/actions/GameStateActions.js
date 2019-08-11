3e80e3c5731e858de0fe2e958dc338bce54b5f24
Steven Ng <40282847+MrStevenNg@users.noreply.github.com>
2019-08-10 21:17:17 -0700

    functionality to display pattern is complete.

--- src/actions/GameStateActions.js
+++ src/actions/GameStateActions.js
@@ -1,4 +1,17 @@
-import { ADD_TO_FORWARD_PATTERN_ARRAY, CLEAR_PATTERNS } from "./types";
+import {
+    RESET_GAME,
+    ADD_TO_FORWARD_PATTERN_ARRAY,
+    CLEAR_PATTERNS,
+    SET_DISPLAY_PATTERN
+} from "./types";
+
+// RESET_GAME ////
+export const resetGame = () => {
+    // console.log(`RESET_GAME action called!`);
+    return {
+        type: RESET_GAME
+    };
+};
 
 // CLEAR_PATTERNS ////
 export const clearPatterns = () => {
@@ -10,9 +23,18 @@ export const clearPatterns = () => {
 
 // ADD_TO_FORWARD_PATTERN_ARRAY ////
 export const addToForwardPatternArray = value => {
-    // console.log(`ADD_TO_FORWARD_PATTERN_ARRAY action called!`);
+    // console.log(`ADD_TO_FORWARD_PATTERN_ARRAY action called! (${value})`);
     return {
         type: ADD_TO_FORWARD_PATTERN_ARRAY,
         payload: value
+    };
+};
+
+// SET_DISPLAY_PATTERN ////
+export const setDisplayPattern = bool => {
+    console.log(`SET_DISPLAY_PATTERN action called! (${bool})`);
+    return {
+        type: SET_DISPLAY_PATTERN,
+        payload: bool
     };
 };
